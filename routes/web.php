ec91ed85823ffea1925a58a6bad284ff2ea4f552
Jhon Hernandez <jhobahego16@hotmail.com>
2024-03-04 10:29:36 -0500

    Add Rutas

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,13 @@
 <?php
 
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\NewsController;
+use App\Http\Controllers\ProjectController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
     return Inertia::render('Welcome', [
@@ -32,4 +26,17 @@ Route::middleware([
     Route::get('/dashboard', function () {
         return Inertia::render('Dashboard');
     })->name('dashboard');
+});
+
+$middlewareGroup = [
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+];
+
+Route::middleware($middlewareGroup)->group(function () {
+    Route::resource('/news', NewsController::class);
+    Route::resource('/projects', ProjectController::class);
+    Route::resource('/admin', AdminController::class);
+    Route::resource('/about', AboutController::class);
 });
