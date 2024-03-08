3d04f41d709a553b5f1825ab605d09d208afe8fc
Jhon Hernandez <jhobahego16@hotmail.com>
2024-03-07 22:48:23 -0500

    Update contenido de la página de inicio del semillero de investigación SUIS

--- resources/js/Components/Index.vue
+++ resources/js/Components/Index.vue
@@ -7,38 +7,45 @@
       <article class="flex flex-col md:flex-row md:gap-x-2 max-w-screen-md mb-28 mt-4 mx-auto items-center">
         <div>
           <h2 class="text-xl md:text-2xl font-bold mb-2">Semillero de investigacion SUIS</h2>
-          <p class="text-sm md:text-xl opacity-80">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatibus,
-            fugit
-            delectus dolore eligendi placeat similique minus sed deserunt. Illo, incidunt
-            ipsum dolor sit, amet consectetur adipisicing elit. Voluptatibus, fugit
-            voluptates alias ex consectetur et ipsum voluptas voluptate voluptatem possimus.
+          <p class="text-sm md:text-xl opacity-80 mb-2">SUIS es un grupo académico conformado por estudiantes y docentes
+            de
+            la
+            facultad de Ingeniería en Sistemas, dedicado a la investigación y desarrollo de proyectos innovadores en el
+            campo de la ingeniería de software, redes de computadoras, inteligencia artificial, entre otros.
           </p>
-          <p class="text-sm md:text-xl opacity-80">Lorem
-            delectus dolore eligendi placeat similique minus sed deserunt. Illo, incidunt
-            voluptates alias ex consectetur et ipsum voluptas voluptate voluptatem possimus..
+          <p class="text-sm md:text-xl opacity-80 mb-2">Nuestro semillero se enfoca en promover el desarrollo de
+            habilidades
+            investigativas en nuestros miembros, fomentando la creatividad y el pensamiento crítico para abordar
+            problemas tecnológicos actuales y futuros. Trabajamos en estrecha colaboración con la industria y otras
+            instituciones académicas para transferir conocimientos y resultados de investigación que impacten
+            positivamente en la sociedad.
           </p>
         </div>
         <img class="rounded-md shadow-md mt-3 md:mt-0" src="/img/carrousel/img/1.png" alt="imagen de sección eventos">
       </article>
 
       <article class="flex flex-col md:flex-row md:gap-x-8 max-w-screen-md mb-28 mx-auto items-center">
-        <img class="rounded-md shadow-md mb-3 md:mb-0" src="/img/carrousel/img/2.png" alt="imagen de sección de noticias">
+        <img class="rounded-md shadow-md mb-3 md:mb-0" src="/img/carrousel/img/2.png"
+          alt="imagen de sección de noticias">
         <div>
-          <h2 class="text-xl md:text-2xl font-bold mb-2">Ultimas Noticias de Semillero de investigacion SUIS</h2>
-          <p class="text-sm md:text-xl opacity-80">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatibus,
-            fugit
-            delectus dolore eligendi placeat similique minus sed deserunt. Illo, incidunt
-            voluptates alias ex consectetur et ipsum voluptas voluptate voluptatem possimus
+          <h2 class="text-xl md:text-2xl font-bold mb-2">Enfocados en mejorar los procesos academicos de la región
+            sucreña</h2>
+          <p class="text-sm md:text-xl opacity-80">Nuestro semillero está abierto a estudiantes de pregrado y posgrado
+            interesados en la investigación en Ingeniería en Sistemas. ¡Únete a nosotros y sé parte del futuro de la
+            tecnología!
           </p>
-          <p class="text-sm md:text-xl opacity-80">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatibus,
-            fugit
-            delectus dolore eligendi placeat similique minus sed deserunt. Illo, incidunt
-            voluptates alias ex consectetur et ipsum voluptas voluptate voluptatem possimus..
+          <p class="text-sm md:text-xl opacity-80">Queremos que la comunidad académica y la sociedad en general conozcan
+            los avances y resultados de nuestras investigaciones. Por eso, te invitamos a participar en nuestros eventos
+            y a leer nuestras publicaciones.
           </p>
         </div>
       </article>
 
-      <Carousel :slides="slides" :interval="4000" controls indicators />
+      <article>
+        <h2 class="text-xl md:text-2xl font-bold mb-2">Galería de imagenes sobre encuentros en los que ha participado
+          nuestro semillero</h2>
+        <Carousel :slides="slides" :interval="4000" controls indicators />
+      </article>
 
     </div>
   </div>
