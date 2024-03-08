a66f2d44844df7c7caacae260a6dc6298d663ef5
Jhon Hernandez <jhobahego16@hotmail.com>
2024-03-07 22:43:50 -0500

    Add campos fillable, hidden y casts a la clase Evento

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -10,4 +10,28 @@ class Event extends Model
 {
     use HasFactory;
     use SoftDeletes;
+
+    protected $fillable = [
+        'user_id',
+        'title',
+        'description',
+        'activity_type',
+        'link',
+        'location',
+        'start_date',
+        'end_date',
+        'duration',
+        'color',
+    ];
+
+    protected $hidden = [
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
+
+    protected $casts = [
+        'start_date' => 'datetime',
+        'end_date' => 'datetime',
+    ];
 }
