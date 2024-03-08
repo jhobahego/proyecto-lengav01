4dc1b97e5491713f92dfc84947434bcc1d290d16
Jhon Hernandez <jhobahego16@hotmail.com>
2024-03-07 22:43:26 -0500

    Add Modelo de usuario y pasar usuarios a la vista de proyectos

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Project;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class ProjectController extends Controller
@@ -12,10 +13,9 @@ class ProjectController extends Controller
      */
     public function index()
     {
+        $users = User::all();
         $projects = Project::all();
-        return inertia('ProjectsView', [
-            'projects' => $projects
-        ]);
+        return inertia('ProjectsView', compact('projects', 'users'));
     }
 
     /**
@@ -36,15 +36,8 @@ class ProjectController extends Controller
         $data['general_objectives'] = json_encode($data['general_objectives']);
         $data['specific_objectives'] = json_encode($data['specific_objectives']);
 
-        $project = Project::create($data);
-
-        if ($project) {
-            return redirect()->route('projects.index')->with('success', [
-                'type' => 'success',
-                'title' => 'Registro creado correctamente',
-                'message' => 'Proyecto creado con éxito'
-            ]);
-        }
+        Project::create($data);
+        return redirect()->route('projects.index');
     }
 
     /**
@@ -56,16 +49,6 @@ class ProjectController extends Controller
     }
 
     /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Project $project)
-    {
-        $project->general_objectives = json_decode($project->general_objectives);
-        $project->specific_objectives = json_decode($project->specific_objectives);
-        return view('projects.edit', compact('project'));
-    }
-
-    /**
      * Update the specified resource in storage.
      */
     public function update(Request $request, Project $project)
@@ -74,11 +57,7 @@ class ProjectController extends Controller
         $request['specific_objectives'] = json_encode($request['specific_objectives']);
 
         $project->update($request->all());
-        return redirect()->route('projects.index')->with('success', [
-            'type' => 'success',
-            'title' => 'Registro actualizado correctamente',
-            'message' => 'Proyecto actualizado con éxito'
-        ]);
+        return redirect()->route('projects.index');
     }
 
     /**
@@ -87,10 +66,6 @@ class ProjectController extends Controller
     public function destroy(Project $project)
     {
         $project->delete();
-        return redirect()->route('projects.index')->with('danger', [
-            'type' => 'success',
-            'title' => 'Registro eliminado correctamente',
-            'message' => 'Proyecto eliminado con éxito'
-        ]);
+        return redirect()->route('projects.index');
     }
 }
