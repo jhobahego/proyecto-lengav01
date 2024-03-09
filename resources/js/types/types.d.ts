72a3af2e7bd8dbde6c2084659783a3a8bac0c09b
Jhon Hernandez <jhobahego16@hotmail.com>
2024-03-08 21:31:47 -0500

    Remove campos innecesarios en los tipos de User

--- resources/js/types/types.d.ts
+++ resources/js/types/types.d.ts
@@ -36,11 +36,6 @@ interface User {
   id: number;
   name: string;
   email: string;
-  email_verified_at: string | null;
-  password: string;
   remember_token: string;
-  current_team_id: number | null;
   profile_photo_path: string | null;
-  created_at: string;
-  updated_at: string;
 }
